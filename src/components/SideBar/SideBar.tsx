7cc734051322b44d6424ab873fd36ebf28b3462b
NataliaAfricano <naaleafri@gmail.com>
2023-09-03 21:00:29 -0500

    Update SlideBar

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -8,35 +8,79 @@ import { BsBox } from 'react-icons/bs';
 import {BsFillBarChartFill} from 'react-icons/bs';
 import { TiContacts } from "react-icons/ti";
 import { MdKeyboardArrowRight,MdKeyboardArrowLeft } from 'react-icons/md';
+import {FiUsers} from 'react-icons/fi';
+import {FiTag } from 'react-icons/fi';
+import {FiSettings} from 'react-icons/fi';
+import {BiCart} from 'react-icons/bi';
+import {BiFileBlank} from 'react-icons/bi';
+import {CiLogout} from 'react-icons/ci';
 import { useSidebarContext } from '@/context/SidebarContext';
 
 import { usePathname } from 'next/navigation';
 
 const sidebarItems = [{
-  name: "Dashboard",
-  href: "/dashboard",
-  icon: MdDashboard,
-},
-{
-  name: "Estadisticas",
-  href: "/dashboard/stats",
-  icon: BsFillBarChartFill,
-},
-{
-  name: "Inventario Plus",
-  href: "/dashboard/inventory_plus",
-  icon: LuMousePointerClick,
-},
-{
-  name: "Usuarios",
-  href: "/dashboard/users",
-  icon: TiContacts,
-},
-{
-  name: "Inventario",
-  href: "/dashboard/inventory",
-  icon: BsBox,
-},
+    name: "Dashboard",
+    href: "/dashboard",
+    icon: MdDashboard,
+  },
+  {
+    name: "Estadisticas",
+    href: "/dashboard/stats",
+    icon: BsFillBarChartFill,
+    text: "ANALITICA",
+  },
+  {
+    name: "Inventario Plus",
+    href: "/dashboard/inventory_plus",
+    icon: LuMousePointerClick,
+  },
+  {
+    name: "Usuarios",
+    href: "/dashboard/users",
+    icon: TiContacts,
+    text: "Administracion",
+  },
+  {
+    name: "Clientes",
+    href: "/dashboard/clients",
+    icon: FiUsers,
+  },
+  {
+    name: "Inventario",
+    href: "/dashboard/inventory",
+    icon: BsBox,
+    text: "INVENTARIO",
+  },
+  {
+    name: "Proveedores",
+    href: "/dashboard/providers",
+    icon: BiCart,
+  },
+  {
+    name: "Reportes",
+    href: "/dashboard/reports",
+    icon: BiFileBlank,
+  },
+  {
+    name: "Marcas y Categorias",
+    href: "/dashboard/category",
+    icon: FiTag,
+  },
+  {
+    name: "Configuracion",
+    href: "/dashboard/settings",
+    icon: FiSettings,
+    text: (<>
+           <br />
+           <br />
+        </>
+    ),
+  },
+  {
+    name: "Salir",
+    href: "/logout",
+    icon: CiLogout,
+  },
 ];
 
 
@@ -49,8 +93,8 @@ export default function SideBar() {
   const { isCollapsed, toggleSidebarcollapse } = useSidebarContext();
 
   console.log(isCollapsed);
-  
-  
+
+
   return (
     <div className={styles.sidebar__wrapper}>
       <button className={styles.btn}
@@ -68,15 +112,19 @@ export default function SideBar() {
           />
           <p className={styles.sidebar__logo_name}>Stock-Pro</p>
         </div>
+        <br />
         <ul className={styles.sidebar__list}>
-          {sidebarItems.map(({ name, href, icon: Icon }) => (
+          {sidebarItems.map(({ name, href, icon: Icon, text}) => (
             <li className={styles.sidebar__item} key={name}>
+              <span className={styles.sidebar__text}>
+                  {text}
+              </span>
               <Link
                 className={ pathname === href ? styles.
-                sidebar__link_active : styles.sidebar__link 
+                sidebar__link_active : styles.sidebar__link
                 }
                 href={href}
-                >    
+                >
                 <div className={styles.sidebar__icon}>
                   <Icon />
                 </div>
@@ -86,7 +134,7 @@ export default function SideBar() {
               </Link>
             </li>
           ))}
-          
+
         </ul>
       </aside>
     </div>
